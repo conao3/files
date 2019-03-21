c1d296a3c72ef9b10a7b05501fdad005b876dcbb
conao3 <conao3@gmail.com>
2019-03-22 06:37:38 +0900

    move created png file

--- clojure/src/conao3/files/mustache.clj
+++ clojure/src/conao3/files/mustache.clj
@@ -1,7 +1,8 @@
 (ns conao3.files.mustache
   (:require [clostache.parser :as mustache]
             [clojure.java.io :as io]
-            [clojure.java.shell :refer [sh]])
+            [clojure.java.shell :refer [sh]]
+            [conao3.files.util :as util])
   (:gen-class))
 
 (defn create-header-svg [name]
@@ -9,14 +10,17 @@
       (spit (mustache/render-resource "header.svg.mustache" {:name name}))))
 
 (defn create-header-png [name]
-  (let [svgfile (io/file (str "../headers/svg" name ".svg"))]
+  (let [svgpath (str "../headers/svg/" name ".svg")
+        pngpath (str "../headers/png/" name ".png")
+        svgfile (io/file svgpath)]
     (when-not (.exists svgfile) (create-header-svg name))
     (sh "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
         "--headless"
         "--disable-gpu"
         "--screenshot=screenshot.png"
         "--window-size=1000,170"
-        (str "file://" (.getCanonicalPath svgfile))))
+        (str "file://" (.getCanonicalPath svgfile)))
+    (util/move-file "./screenshot.png" pngpath)))
 
 (defn create-header [name]
   (create-header-svg name)
