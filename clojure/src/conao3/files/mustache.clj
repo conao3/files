cea6053a6485d2ac2f06f496d2af7c3b2190aba9
conao3 <conao3@gmail.com>
2019-03-22 06:31:29 +0900

    implement create-heaader-png function

--- clojure/src/conao3/files/mustache.clj
+++ clojure/src/conao3/files/mustache.clj
@@ -1,12 +1,22 @@
 (ns conao3.files.mustache
-  (:require [clostache.parser :as mustache])
+  (:require [clostache.parser :as mustache]
+            [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]])
   (:gen-class))
 
 (defn create-header-svg [name]
   (-> (str "../headers/svg/" name ".svg")
       (spit (mustache/render-resource "header.svg.mustache" {:name name}))))
 
-(defn create-header-png [name])
+(defn create-header-png [name]
+  (let [svgfile (io/file (str "../headers/svg" name ".svg"))]
+    (when-not (.exists svgfile) (create-header-svg name))
+    (sh "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+        "--headless"
+        "--disable-gpu"
+        "--screenshot=screenshot.png"
+        "--window-size=1000,170"
+        (str "file://" (.getCanonicalPath svgfile))))
 
 (defn create-header [name]
   (create-header-svg name)
