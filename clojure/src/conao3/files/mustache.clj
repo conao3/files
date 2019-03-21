2af72b966858298f96736eedd4b876f7413f7271
conao3 <conao3@gmail.com>
2019-03-22 07:21:47 +0900

    fix google chrome exec command

--- clojure/src/conao3/files/mustache.clj
+++ clojure/src/conao3/files/mustache.clj
@@ -1,5 +1,6 @@
 (ns conao3.files.mustache
   (:require [clostache.parser :as mustache]
+            [clojure.string :as string]
             [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]
             [conao3.files.util :as util])
@@ -14,12 +15,15 @@
         pngpath (str "../header/png/" name ".png")
         svgfile (io/file svgpath)]
     (when-not (.exists svgfile) (create-header-svg name))
-    (sh "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
-        "--headless"
-        "--disable-gpu"
-        "--screenshot=screenshot.png"
-        "--window-size=1000,170"
-        (str "file://" (.getCanonicalPath svgfile)))
+    (sh "bash" "-c"
+        (string/join
+         " "
+         ["/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
+          "--headless"
+          "--disable-gpu"
+          "--screenshot=screenshot.png"
+          "--window-size=1000,170"
+          (str "file://" (.getCanonicalPath svgfile))]))
     (util/move-file "./screenshot.png" pngpath)))
 
 (defn create-header [name]
